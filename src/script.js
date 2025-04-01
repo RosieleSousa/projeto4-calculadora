b3182b739a587567cfaba70ab8a738f19676185f
Rosiele Sousa <rosielesousa49@gmail.com>
2025-04-01 08:22:46 -0300

    up

--- src/script.js
+++ src/script.js
@@ -20,3 +20,17 @@ function calculate() {
         display.value = 'Erro';
     }
 }
+
+const themeToggleButton = document.getElementById("theme-toggle");
+themeToggleButton.addEventListener("click", function () {
+    const body = document.body;
+    const currentTheme = body.getAttribute("data-theme");
+
+    if (currentTheme === "light") {
+        body.setAttribute("data-theme", "dark");
+        themeToggleButton.textContent = "☀️ Alternar Tema";
+    } else {
+        body.setAttribute("data-theme", "light");
+        themeToggleButton.textContent = "🌙 Alternar Tema";
+    }
+});
